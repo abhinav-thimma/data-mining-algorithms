f03ff15fd6da5f8d2cb6015055ca7ba2a8b9d3a0
tabhi0797 <abhinav071197@gmail.com>
2018-11-23 09:55:19 +0530

    Update 5 point summary.py

--- 5 point summary/5 point summary.py
+++ 5 point summary/5 point summary.py
@@ -15,7 +15,7 @@ def printBoxPlot(minEle, q1, median, q3, maxEle):
 data = [int(x) for x in input().split(',')]
 minEle = min(data)
 q1 = data[int(len(data)/4)]
-median = data[int(len(data)/2)] if(len(data)%2 == 0) else (data[int((len(data)+1)/2)]+data[int((len(data)-1)/2)])/2
+median = data[int(len(data)/2)] if(len(data)%2 != 0) else (data[int((len(data)+1)/2)]+data[int((len(data)-1)/2)])/2
 q3 = data[int(len(data)*3/4)]
 maxEle = max(data)
 
